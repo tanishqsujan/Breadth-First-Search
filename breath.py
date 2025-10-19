f128d85bb207ba24527fbc4559180e23ea340b34
tanishqsujan <tanishq.kothamasu@gmail.com>
2025-10-19 16:21:13 +0400

    Implementation of BFS using Python

---
+++ breath.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+    
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=" ")
+        
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+                
+graph = {
+    'A' : ['B', 'C'],
+    'B' : ['A', 'D', 'E'],
+    'C' : ['A', 'F'],
+    'D' : ['B'],
+    'E' : ['B', 'F'],
+    'F' : ['C', 'E']
+}
+
+print("BFS Traversal: ")
+bfs(graph, 'A')
